e601ba65554812e7bf1872fda950a543c04b375f
devmits/abbas.joda <absyinka@gmail.com>
2024-08-16 01:46:33 +0100

    Fixed terraform format error

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,11 +13,11 @@ variable "ecs_desired_count" {
 variable "dockerhub_username" {
   description = "Docker Hub username"
   type        = string
-  sensitive = true
+  sensitive   = true
 }
 
 variable "dockerhub_token" {
   description = "Docker Hub access token"
   type        = string
-  sensitive = true
+  sensitive   = true
 }
