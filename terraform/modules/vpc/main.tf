1aa2554fde364f4fc79e24440697a0807e88f9a7
devmits/abbas.joda <absyinka@gmail.com>
2024-08-21 18:08:13 +0100

    Removed default_route resource

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -61,18 +61,18 @@ resource "aws_route_table_association" "public_subnet_2_association" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
+# resource "aws_route" "default_route" {
+#   route_table_id         = aws_route_table.public.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.this.id
 
-  lifecycle {
-    ignore_changes = [
-      destination_cidr_block,
-      gateway_id,
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       destination_cidr_block,
+#       gateway_id,
+#     ]
+#   }
+# }
 
 resource "aws_route_table_association" "subnet1" {
   subnet_id      = aws_subnet.subnet1.id
